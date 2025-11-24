43af2c30aa15fc300019632a3a97bcc703264b15
Suvo Ghosh <suvosh663@gmail.com>
2025-11-24 14:58:17 +0530

    delete some file

--- server/app.js
+++ server/app.js
@@ -5,8 +5,6 @@ import connectDB from "./configs/mongooseConnection.js";
 import authRoutes from "./routes/authRoutes.js";
 import adminUserRoutes from "./routes/adminUserRoutes.js";
 import { seedSuperAdmin } from "./utils/seedSuperAdmin.js";
-// import salaryRoutes from "./routes/salaryRoutes.js";
-import payrollRoutes from "./routes/payrollRoutes.js";
 
 dotenv.config();
 
@@ -25,8 +23,6 @@ app.use(cors({
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminUserRoutes);
-// app.use("/api/salary", salaryRoutes);
-app.use("/api/payroll", payrollRoutes);
 
 app.get("/", (req, res) => {
     res.json({ ok: true, message: "EMS + Payroll API is running" });
